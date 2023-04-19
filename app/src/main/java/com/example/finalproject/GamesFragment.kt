e59b4fdb50f3ffef0f69f722578e0dcf19de7f66
ebartell24 <bartelle24@hsnet.ahsd.org>
2023-04-19 13:01:37 -0400

    set up day 1

---
+++ app/src/main/java/com/example/finalproject/GamesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalproject.databinding.FragmentRosterBinding
+
+
+class GamesFragment : Fragment() {
+    private var _binding: FragmentRosterBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRosterBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        return rootView
+    }
+
+}
