0cae069d15dc1885de0967c87c535245d98b89ea
ebartell24 <bartelle24@hsnet.ahsd.org>
2023-04-26 12:59:11 -0400

    april 26th progress

--- app/src/main/java/com/example/finalproject/CoachingFragment.kt
+++ app/src/main/java/com/example/finalproject/CoachingFragment.kt
@@ -1,5 +1,7 @@
 package com.example.finalproject
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -23,6 +25,35 @@ class CoachingFragment : Fragment() {
 
         val args = CoachingFragmentArgs.fromBundle(requireArguments())
         binding.welcome.text = "Welcome ${args.nameArg}"
+
+        binding.link.setOnClickListener{
+            openWebPage(R.string.website.toString())
+        }
+
+        binding.call.setOnClickListener{
+            dialPhoneNumber(R.string.phoneNumber.toString())
+        }
+
         return rootView
     }
+
+    fun openWebPage(url:String){
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun dialPhoneNumber(phoneNumber: String){
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            startActivity(intent)
+        }
+    }
+
 }
+
+
---
+++ app/src/main/java/com/example/finalproject/Roster.kt
@@ -0,0 +1,4 @@
+package com.example.finalproject
+
+data class Roster(val name: String, val number: Int, val position: String, val gradYear: Int) {
+}
