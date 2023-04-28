de87ca67d74d7f0eb021a67042575873c4d25b65
ebartell24 <bartelle24@hsnet.ahsd.org>
2023-04-28 13:01:21 -0400

    april 28th progress

---
+++ app/src/main/java/com/example/finalproject/Games.kt
@@ -0,0 +1,5 @@
+package com.example.finalproject
+
+data class Games(val opponent: String, val time: String, val score: String, val winLose: String) {
+}
+
--- app/src/main/java/com/example/finalproject/RosterFragment.kt
+++ app/src/main/java/com/example/finalproject/RosterFragment.kt
@@ -24,6 +24,23 @@ class RosterFragment : Fragment() {
 
         val args = RosterFragmentArgs.fromBundle(requireArguments())
         binding.welcome3.text = "Welcome ${args.nameArg}"
+
+        val roster = listOf(Roster("Emily Bartell", 9, "Defense", "Class Of 2024"),
+            Roster("Sadie Bonczek", 2, "Defense", "Class Of 2023"),
+            Roster("Bella DeRiggi", 22, "Forward", "Class Of 2026"),
+            Roster("Kaylyn Elliott", 18, "Defense", "Class Of 2024"),
+            Roster("Amelia Gonzalez", 24, "Midfield", "Class Of 2025"),
+            Roster("Grace Keris", 21, "Defense", "Class Of 2026"),
+            Roster("Emma Newman", 33, "Goalie", "Class Of 2024"),
+            Roster("Caroline Pease", 1, "Forward", "Class Of 2023"),
+            Roster("Olivia Piasecki", 5, "Defense", "Class Of 2024"),
+            Roster("Sofia Policare", 26, "Midfield", "Class Of 2025"),
+            Roster("Marygrace Sabatini", 3, "Midfield", "Class Of 2024"),
+            Roster("Caly Yankow", 14, "Forward", "Class Of 2024"),
+            Roster("Maddie Zalewski", 6, "Midfield", "Class Of 2024"))
+
+        val mAdaptor = rosterAdapter(roster)
+        binding.recyclerView.adapter = mAdaptor
         return rootView
     }
 
