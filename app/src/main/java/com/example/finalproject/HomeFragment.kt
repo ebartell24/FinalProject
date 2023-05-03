4fa412792b50b5b7559344a47e07ba41b2ff24b3
ebartell24 <bartelle24@hsnet.ahsd.org>
2023-05-03 13:01:51 -0400

    may 3rd progress

--- app/src/main/java/com/example/finalproject/HomeFragment.kt
+++ app/src/main/java/com/example/finalproject/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.finalproject.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -23,16 +25,19 @@ class HomeFragment : Fragment() {
         binding.coaching.setOnClickListener(){
             val guest = binding.enterName.text.toString()
             val action = HomeFragmentDirections.actionHomeFragmentToCoachingFragment(guest)
+            Snackbar.make(binding.coaching, R.string.coachSnackBar, Snackbar.LENGTH_SHORT).show()
             rootView.findNavController().navigate(action)
         }
         binding.roster.setOnClickListener(){
             val guest = binding.enterName.text.toString()
             val action = HomeFragmentDirections.actionHomeFragmentToRosterFragment(guest)
+            Toast.makeText(activity, R.string.rosterToast, Toast.LENGTH_SHORT).show()
             rootView.findNavController().navigate(action)
         }
         binding.games.setOnClickListener(){
             val guest = binding.enterName.text.toString()
             val action = HomeFragmentDirections.actionHomeFragmentToGamesFragment(guest)
+            Toast.makeText(activity, R.string.gamesToast, Toast.LENGTH_SHORT).show()
             rootView.findNavController().navigate(action)
         }
 
